8a7edd5f24d31d8d022f55dc940e4eb14f9372e7
Mike Woelmer <mike.woelmer@atomicobject.com>
2016-06-10 08:50:10 -0400

    Initial Commit

---
+++ ExploringUIStackViewDistribution/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ExploringUIStackViewDistribution
+//
+//  Created by Mike Woelmer on 6/10/16.
+//  Copyright © 2016 atomicobject. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ExploringUIStackViewDistribution/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ExploringUIStackViewDistribution
+//
+//  Created by Mike Woelmer on 6/10/16.
+//  Copyright © 2016 atomicobject. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
